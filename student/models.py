ebf97ca846f829df2913fefa65dbdb0a99678e36
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-01-23 21:01:42 +0500

    Created student model and migrated.

--- student/models.py
+++ student/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    rol_no = models.TextField()
+    name = models.TextField(max_length=40)
+    stud_class = models.TextField()
+    department = models.TextField()
